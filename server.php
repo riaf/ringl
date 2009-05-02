b20fa639494af1577ac7a58d6276856efef253cf
Keisuke SATO <riafweb@gmail.com>
2009-05-02 15:24:59 +0900

    request

--- server.php
+++ server.php
@@ -2,10 +2,12 @@
 declare(ticks = 1);
 require dirname(__FILE__). '/__settings__.php';
 import('core.Log');
+import('core.Request');
 Log::disable_display();
 import('Chat.ChatServer');
 
-$object_list = C(ChatMessage)->find_all(Q::gt('id', $this->inVars('since_id', 0)), Q::order('-id'));
+$req = new Request();
+$object_list = C(ChatMessage)->find_all(Q::gt('id', $req->inVars('since_id', 0)), Q::order('-id'));
 if(count($object_list) > 0){
     echo ChatServer::models_to_json($object_list);
     exit;
