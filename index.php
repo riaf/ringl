cf8ebb78832c570da68d13e99939b4d413a45650
Keisuke SATO <riafweb@gmail.com>
2009-05-02 15:14:17 +0900

    fix

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ import('Chat');
 $flow = new Flow();
 $flow->handler(array(
     '^/archives' => 'class=Chat,method=models,template=archive.html',
-    '^/get_messages' => 'class=Chat,method=models_json_pool',
     '^/say' => 'class=Chat,method=say',
     '' => 'class=Chat,method=models,template=index.html',
 ));
--- server.php
+++ server.php
@@ -2,8 +2,14 @@
 declare(ticks = 1);
 require dirname(__FILE__). '/__settings__.php';
 import('core.Log');
-// Log::disable_display();
+Log::disable_display();
 import('Chat.ChatServer');
+
+$object_list = C(ChatMessage)->find_all(Q::gt('id', $this->inVars('since_id', 0)), Q::order('-id'));
+if(count($object_list) > 0){
+    echo ChatServer::models_to_json($object_list);
+    exit;
+}
 
 pcntl_signal(SIGUSR1, array(C(ChatServer), 'models_json'));
 sleep(def('chat@timeout'));
